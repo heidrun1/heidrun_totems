41ed97774864510e896ebea3f75845c58c62ea9b
heidrun <129896999+heidrun1@users.noreply.github.com>
2024-03-27 12:31:37 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,16 +1,16 @@
 Config = {}
 
 Config.Totems = {
-    -- Defines a new totem with a unique key. In this example, the key is "example".
+    -- Defines a new totem with a unique key. In this example, the key is "example_item".
     ["example_item"] = {
-        -- Defines the interval in milliseconds between each effect activation. In this example, the interval is 2000ms or 2 seconds.
+        -- Defines the interval in milliseconds between each effect activation.
         interval = 2000,
 
-        -- Defines the function to be executed when the totem effect is activated. In this example, the function resets the player's stamina and sets their sprint multiplier to 1.4.
+        -- Defines the function to be executed when the totem effect is activated.
         action = function(playerPed, playerId)
         end,
 
-        -- Defines the function to be executed when the totem effect is removed. In this example, the function sets the player's sprint multiplier back to 1.0.
+        -- Defines the function to be executed when the totem effect is removed. 
         remove = function(playerPed, playerId)
         end
     },
