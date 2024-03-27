aa269eee604f03027f85b5c2d29c29f1b1d10303
heidrun <129896999+heidrun1@users.noreply.github.com>
2024-03-27 12:27:22 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,6 @@ lua54 'yes'
 author 'Heidrun'
 description 'https://github.com/heidrun1'
 
-shared_scripts { "config.lua", '@es_extended/imports.lua', }
+shared_scripts { 'config.lua', '@es_extended/imports.lua', }
 client_script 'client.lua'
 server_scripts { '@oxmysql/lib/MySQL.lua', 'server.lua' }
